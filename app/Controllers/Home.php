b71c64acda25c2219305dfc6147630bb41c444fe
Silvanus <silvanusprasetyo@gmail.com>
2025-06-24 21:01:15 +0700

    menambahkan api data transaksi pembelian

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Models\ProdukModel;
+use App\Models\TransactionDetailModel;
+use App\Models\TransactionModel;
 
 class Home extends BaseController
 {
@@ -11,10 +13,16 @@ class Home extends BaseController
     //     return view('welcome_message');
     // }
     protected $product;
+    protected $transaction;
+    protected $transaction_detail;
 
     function __construct()
     {
+        helper('number');
+        helper('form');
         $this->product = new ProdukModel();
+        $this->transaction = new TransactionModel();
+        $this->transaction_detail = new TransactionDetailModel();
     }
 
 
@@ -26,6 +34,31 @@ class Home extends BaseController
 
         return view('v_home', $data);
     }
+
+    public function profile()
+{
+    $username = session()->get('username');
+    $data['username'] = $username;
+
+    $buy = $this->transaction->where('username', $username)->findAll();
+    $data['buy'] = $buy;
+
+    $product = [];
+
+    if (!empty($buy)) {
+        foreach ($buy as $item) {
+            $detail = $this->transaction_detail->select('transaction_detail.*, product.nama, product.harga, product.foto')->join('product', 'transaction_detail.product_id=product.id')->where('transaction_id', $item['id'])->findAll();
+
+            if (!empty($detail)) {
+                $product[$item['id']] = $detail;
+            }
+        }
+    }
+
+    $data['product'] = $product;
+
+    return view('v_profile', $data);
+}
 
     public function faq(): string
     {
